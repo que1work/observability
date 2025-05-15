f2039ca7aba322f204df1c24e687a0796f2ce068
queworks <danilkondratev954@gmail.com>
2025-05-15 15:06:25 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+const register = client.register;
+
+// Створимо базові метрики
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [50, 100, 200, 300, 400, 500]
+});
+
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.route ? req.route.path : req.path, code: res.statusCode });
+  });
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js with Prometheus metrics!');
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
